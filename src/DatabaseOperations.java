ef9bc30f400cb364c86e8d2e2290d80956b84710
shreyasrai2003 <109197258+shreyasrai2003@users.noreply.github.com>
2023-05-03 17:27:06 +0530

    Create DatabaseOperations.java class
    Method Declarations

---
+++ src/DatabaseOperations.java
@@ -0,0 +1,50 @@
+/*
+Name: Shreyas Rai
+PRN: 21070126088
+Batch: AIML B1
+OS: MacOS 12 Monterey version 12.6.2
+
+java Version: 19.0.1 2022-10-18
+Java(TM) SE Runtime Environment (build 19.0.1+10-21)
+Java HotSpot(TM) 64-Bit Server VM (build 19.0.1+10-21, mixed mode, sharing)
+*/
+
+public class DatabaseOperations
+{
+
+    // Method to create a new table
+    public static void createTable(String tableName, String[] columnNames) 
+    {
+        
+    }
+
+    // Method to drop a table
+    public static void dropTable(String tableName) 
+    {
+        
+    }
+
+    // Method to add a new row to a table
+    public static void addRow(String tableName, String[] columnNames, String[] values) 
+    {
+        
+    }
+
+    // Method to view all tables in the database
+    public static void viewTables() 
+    {
+        
+    }
+
+    // Method to delete a row from a table
+    public static void deleteRow(String tableName, String condition) 
+    {
+
+    }
+
+    //Method to view contents of a table
+    public static void viewTableContent(String tableName)
+    {
+        
+    }
+}
