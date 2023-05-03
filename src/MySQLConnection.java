e3beb421018f2796e094e39dbe6bc4b893d0cfaf
shivangisingh27 <89804966+shivangisingh27@users.noreply.github.com>
2023-05-03 18:37:23 +0530

    Update and rename MySQLConnection to MySQLConnection.java

---
+++ src/MySQLConnection.java
@@ -0,0 +1,18 @@
+// #Name - Shivangi Singh
+// #PRN 21070126085
+
+
+public class MySQLConnection {
+ private static Connection conn = null;
+
+ public static void establishConnection() {
+ 
+ }
+ 
+ public static Connecreturn conn;
+ }
+ public static void closeConnection() {
+ 
+ }
+
+}
