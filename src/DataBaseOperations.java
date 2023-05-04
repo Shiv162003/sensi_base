625bfc401c327dcaf6300f85148e870399711dd2
shreyasrai2003 <109197258+shreyasrai2003@users.noreply.github.com>
2023-05-04 11:25:39 +0530

    Update DataBaseOperations.java

--- src/DataBaseOperations.java
+++ src/DataBaseOperations.java
@@ -1,3 +1,13 @@
+/*
+Name: Shreyas Rai
+PRN: 21070126088
+Batch: AIML B1
+OS: MacOS 12 Monterey version 12.6.2
+java Version: 19.0.1 2022-10-18
+Java(TM) SE Runtime Environment (build 19.0.1+10-21)
+Java HotSpot(TM) 64-Bit Server VM (build 19.0.1+10-21, mixed mode, sharing)
+*/
+
 import java.sql.*;
 public class DataBaseOperations extends MySQLConnection {
     static MySQLConnection ob =new MySQLConnection();
